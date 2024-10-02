d3bc0d2679ff2f251f231a6e75844bd761cbecad
samuelpy1 <samuelppy1@gmail.com>
2024-10-02 20:39:19 -0300

    limpando e formantando pastas/arrumando links

--- src/app/pages/opcao01/page.tsx
+++ src/app/pages/opcao01/page.tsx
@@ -1,6 +1,6 @@
 export default function opcao01({ params }: { params: { title: string, url: string, serviceVersion: string, midiaType: string, explanation: string, data: string } }) {
     return (
-        <div></div>
+        <div>teste</div>
 
 
 
--- src/app/pages/opcao04/page.tsx
+++ src/app/pages/opcao04/page.tsx
@@ -1,3 +1,3 @@
 export default function opcao04() {
-    return(<div></div>)
+    return (<div></div>)
 }
