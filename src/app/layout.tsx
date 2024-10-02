329b014c4f1f084c5e0d918d48701a0e3fbc8bc7
lemos000 <lemosgabriel2016@gmail.com>
2024-10-02 18:51:52 -0300

    :tada: Commit inicial - Limpeza do boyler plate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+export const viewport: Viewport = {
+  viewportFit: "auto"  
+
+}
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,10 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-br">
+      <body>
         {children}
       </body>
     </html>
