b0fe361d4e640b0920f40d60dda6bb2b414078d9
samuelpy1 <samuelppy1@gmail.com>
2024-10-02 19:54:28 -0300

    so burro n sei pq a imagem n ta funcionando :(

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NotFound() {
     return (
-        <div>
-            <h1>404: Page Not Found</h1>
-            <p>O conteúdo que você está tentando acessar não está disponível!</p>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4x1 font-bold text-gray-800 mb-6">404: Page Not Found</h1>
             <Image src="/img/notfoundpage.png" alt="page not found nasa" width={50} height={50} priority={true} />
+            <p className="text-lg text-gray-600 mb-4">O conteúdo que você está tentando acessar não está disponível!</p>
+            <Link href="/" className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">
+                Voltar a página inicial
+            </Link>
         </div>
     )
 }
