9e4578e16f049a1c370859be453da9da156619bb
samuelpy1 <samuelppy1@gmail.com>
2024-10-02 20:50:40 -0300

    arrumando nome da pasta

---
+++ src/app/pages/opcao04/page.tsx
@@ -0,0 +1,8 @@
+export default function opcao04() {
+    return (
+        <div>
+            <h1>Impacto e Legado</h1>
+            <p>As ideias de Velikovsky enfrentaram resistência significativa da comunidade científica, que as classificou como pseudociência devido à falta de evidências empíricas e à violação de princípios fundamentais da física. Contudo, sua obra conquistou um público popular expressivo, intrigado por suas teorias ousadas e pela narrativa de catástrofes. Velikovsky deixou um legado cultural duradouro, inspirando debates sobre a intersecção entre ciência e mitologia. Mesmo após sua morte, seus conceitos continuam a ser explorados por entusiastas e pesquisadores, refletindo uma resistência à aceitação completa no mainstream científico. Ele permanece uma figura polarizadora, desafiando a rigidez das teorias estabelecidas e ressaltando a importância de questionar e testar continuamente o conhecimento científico. A ousadia de suas hipóteses continua a ser um ponto de referência para aqueles que buscam explorar os limites do pensamento científico e histórico.</p>
+        </div>
+    )
+}
