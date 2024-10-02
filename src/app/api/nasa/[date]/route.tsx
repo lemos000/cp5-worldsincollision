0b9b14e97dbe7333f6e222a666d858ab428d243a
lemos000 <lemosgabriel2016@gmail.com>
2024-10-02 20:50:17 -0300

    fix: Ajuste da api

--- src/app/api/nasa/[date]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { API_KEY, URL } from "@/data/global";
-
-export async function GET({params}: {params: {date: string}}) {
-
-    const dados = await fetch(URL + API_KEY + params.date)
-    return NextResponse.json(dados);
-
-}
--- src/data/global.ts
+++ src/data/global.ts
@@ -1,2 +1,3 @@
-export const API_KEY = '?api_key=jo5M1IVSJ40d03D3IuL0DYbgkIrQxvOViMoO8IMs'
-export const URL = "https://api.nasa.gov/planetary/apod"
+export const API_KEY = 'jo5M1IVSJ40d03D3IuL0DYbgkIrQxvOViMoO8IMs'
+export const URL = "https://api.nasa.gov/planetary/apod/"
+export const API_URL = "http://localhost:3000/api/nasa/"
