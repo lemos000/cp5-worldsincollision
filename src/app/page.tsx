e1c1fa61a52f4f27d1af7d0606eb4e8f587b8d5f
pyz1m <samuelyariwake@gmail.com>
2024-10-05 01:33:20 -0300

    efeitos e correção de cores do not found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import Nasa from "../public/img/nasa-logo.png";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -8,26 +10,102 @@ export default function Home() {
       <Image src={Nasa} alt="Nasa logo" className="mb-4 h-40 w-auto" />
       <h1 className="text-4xl font-bold text-center mb-2">Worlds in Collision</h1>
       <h2 className="text-2xl text-center mb-8">Immanuel Velikovsky</h2>
-      <div className="flex gap-4">
+      <div className="flex gap-8">
         <Link href="/pages/opcao1/page.tsx">
-          <div className="border-2 border-transparent bg-gray-800 p-4 rounded-lg text-center flex items-center justify-center cursor-pointer hover:border-red-600 hover:bg-blue-900 transition duration-300">
-            🚀 Opção 1
-          </div>
+        <motion.div
+      className="bg-white/10 rounded-lg p-5 backdrop-blur-sm"
+      initial={{ y: 30, opacity: 0, scale: 0.8 }}
+      animate={{
+        y: [0, -10, 0],
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        ease: 'easeInOut',
+        duration: 1.5
+      }}
+      whileHover={{
+        scale: 1.05, // Leve aumento no tamanho
+        boxShadow: '0 0 25px rgba(0, 175, 255, 0.9), 0 0 50px rgba(0, 175, 255, 0.7)',
+      }}
+      style={{
+        boxShadow: '0 0 15px rgba(0, 175, 255, 0.5), 0 0 30px rgba(0, 175, 255, 0.3)',
+      }}
+    >
+            🌌 Opção 1
+          </motion.div>
         </Link>
         <Link href="/pages/opcao2/page.tsx">
-          <div className="border-2 border-transparent bg-gray-800 p-4 rounded-lg text-center flex items-center justify-center cursor-pointer hover:border-red-600 hover:bg-blue-900 transition duration-300">
-            🚀 Opção 2
-          </div>
+        <motion.div
+      className="bg-white/10 rounded-lg p-5 backdrop-blur-sm"
+      initial={{ y: 30, opacity: 0, scale: 0.8 }}
+      animate={{
+        y: [0, -10, 0],
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        ease: 'easeInOut',
+        duration: 1.5
+      }}
+      whileHover={{
+        scale: 1.05, // Leve aumento no tamanho
+        boxShadow: '0 0 25px rgba(0, 175, 255, 0.9), 0 0 50px rgba(0, 175, 255, 0.7)',
+      }}
+      style={{
+        boxShadow: '0 0 15px rgba(0, 175, 255, 0.5), 0 0 30px rgba(0, 175, 255, 0.3)',
+      }}
+    >
+            🌌 Opção 2
+          </motion.div>
         </Link>
         <Link href="/pages/opcao3/page.tsx">
-          <div className="border-2 border-transparent bg-gray-800 p-4 rounded-lg text-center flex items-center justify-center cursor-pointer hover:border-red-600 hover:bg-blue-900 transition duration-300">
-            🚀 Opção 3
-          </div>
+        <motion.div
+      className="bg-white/10 rounded-lg p-5 backdrop-blur-sm"
+      initial={{ y: 30, opacity: 0, scale: 0.8 }}
+      animate={{
+        y: [0, -10, 0],
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        ease: 'easeInOut',
+        duration: 1.5
+      }}
+      whileHover={{
+        scale: 1.05, // Leve aumento no tamanho
+        boxShadow: '0 0 25px rgba(0, 175, 255, 0.9), 0 0 50px rgba(0, 175, 255, 0.7)',
+      }}
+      style={{
+        boxShadow: '0 0 15px rgba(0, 175, 255, 0.5), 0 0 30px rgba(0, 175, 255, 0.3)',
+      }}
+    >
+            🌌 Opção 3
+          </motion.div>
         </Link>
         <Link href="/pages/opcao4/page.tsx">
-          <div className="border-2 border-transparent bg-gray-800 p-4 rounded-lg text-center flex items-center justify-center cursor-pointer hover:border-red-600 hover:bg-blue-900 transition duration-300">
-            🚀 Opção 4
-          </div>
+        <motion.div
+      className="bg-white/10 rounded-lg p-5 backdrop-blur-sm"
+      initial={{ y: 30, opacity: 0, scale: 0.8 }}
+      animate={{
+        y: [0, -10, 0],
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        ease: 'easeInOut',
+        duration: 1.5
+      }}
+      whileHover={{
+        scale: 1.05, // Leve aumento no tamanho
+        boxShadow: '0 0 25px rgba(0, 175, 255, 0.9), 0 0 50px rgba(0, 175, 255, 0.7)',
+      }}
+      style={{
+        boxShadow: '0 0 15px rgba(0, 175, 255, 0.5), 0 0 30px rgba(0, 175, 255, 0.3)',
+      }}
+    >
+            🌌 Opção 4
+          </motion.div>
         </Link>
       </div>
     </div>
