ad851f4ebc42a9911d92697521ef8d3987070eb1
pyz1m <samuelyariwake@gmail.com>
2024-10-05 06:42:01 -0300

    estilização do not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,8 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import nf from "../public/img/nasa-logo.png";
+import { motion } from "framer-motion";
 
 export default function NotFound() {
     return (
@@ -8,8 +10,29 @@ export default function NotFound() {
             <h1 className="text-4x1 font-bold text-red-800 mb-6">404: Page Not Found</h1>
             <Image src={nf} alt="page not found nasa" className="mb-4 h-40 w-auto" />
             <p className="text-lg mb-4">O conteúdo que você está tentando acessar não está disponível!</p>
-            <Link href="/" className="px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition duration-300">
+            <Link href="/">
+            <motion.div
+      className="bg-white/10 rounded-lg p-5 backdrop-blur-sm"
+      initial={{ y: 30, opacity: 0, scale: 0.8 }}
+      animate={{
+        y: [0, -10, 0],
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        ease: 'easeInOut',
+        duration: 1.5
+      }}
+      whileHover={{
+        scale: 1.05,
+        boxShadow: '0 0 25px rgba(0, 175, 255, 0.9), 0 0 50px rgba(0, 175, 255, 0.7)',
+      }}
+      style={{
+        boxShadow: '0 0 15px rgba(0, 175, 255, 0.5), 0 0 30px rgba(0, 175, 255, 0.3)',
+      }}
+    >
                 Voltar a página inicial
+                </motion.div>
             </Link>
         </div>
     )
